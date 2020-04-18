cd7f3627f55137ba3035c55b971de8b52280be92
koyo-miyamura <nexus9071@gmail.com>
2020-04-18 13:24:16 +0900

    Cellの実装

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,11 +1,14 @@
-import React, { FC } from "react";
+import React, { FC, CSSProperties } from "react";
 
-const Cell: FC<{}> = () => {
-  return (
-    <>
-      <button className="square"></button>
-    </>
-  );
+type CellProps = {
+  alive?: boolean;
+};
+
+const Cell: FC<CellProps> = ({ alive = false }) => {
+  const style: CSSProperties = {
+    background: alive ? "#000" : "#FFF",
+  };
+  return <button className="square" style={style}></button>;
 };
 
 export default Cell;
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -5,19 +5,19 @@ const Home: FC<{}> = () => {
   return (
     <>
       <div className="board-row">
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell alive />
+        <Cell alive />
+        <Cell alive />
       </div>
       <div className="board-row">
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell alive />
+        <Cell alive />
+        <Cell alive />
       </div>
       <div className="board-row">
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell alive />
+        <Cell alive />
+        <Cell alive />
       </div>
     </>
   );
