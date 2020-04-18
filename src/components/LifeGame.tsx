af45855fb909031c8ef967f4ae603df66f818efe
koyo-miyamura <nexus9071@gmail.com>
2020-04-19 01:27:09 +0900

    ライフゲームのロジックを書いた

--- src/components/LifeGame.tsx
+++ src/components/LifeGame.tsx
@@ -1,24 +1,58 @@
-import React, { FC, useState, useEffect, useMemo } from "react";
+import React, { FC, useState, useEffect } from "react";
 import Board from "./Board";
 
-const defaultCells: boolean[][] = [
-  [true, true, true],
-  [true, false, true],
-  [true, true, true],
-];
+const aliveCellNum = (cells: boolean[][], i: number, j: number): number => {
+  const aroundCells = [
+    cells[i - 1] ? cells[i - 1][j - 1] : false,
+    cells[i - 1] ? cells[i - 1][j] : false,
+    cells[i - 1] ? cells[i - 1][j + 1] : false,
+    cells[i] ? cells[i][j - 1] : false,
+    cells[i] ? cells[i][j + 1] : false,
+    cells[i + 1] ? cells[i + 1][j - 1] : false,
+    cells[i + 1] ? cells[i + 1][j] : false,
+    cells[i + 1] ? cells[i + 1][j + 1] : false,
+  ];
+
+  return aroundCells.filter((v) => v).length;
+};
+
+const isAlive = (
+  alive: boolean,
+  cells: boolean[][],
+  i: number,
+  j: number
+): boolean => {
+  switch (aliveCellNum(cells, i, j)) {
+    case 0:
+    case 1:
+      return false;
+    case 2:
+      return alive;
+    case 3:
+      return true;
+    default:
+      return false;
+  }
+};
 
 const nextCells = (cells: boolean[][]): boolean[][] => {
-  return cells.map((row) => row.map((alive) => !alive));
+  return cells.map((row, i) =>
+    row.map((alive, j) => isAlive(alive, cells, i, j))
+  );
 };
+
+const defaultCells: boolean[][] = [
+  [false, true, false],
+  [false, true, false],
+  [false, true, false],
+];
 
 const LifeGame: FC<{}> = () => {
   const [cells, setCells] = useState(defaultCells);
-  useMemo(() => cells, [cells]);
 
   useEffect(() => {
     const id = setInterval(() => {
-      const afterCells = nextCells(cells);
-      setCells(afterCells);
+      setCells(nextCells(cells));
     }, 1000);
 
     return () => {
