e85227eee83de034ad4b0c25566dabfe07618f98
koyo-miyamura <nexus9071@gmail.com>
2020-05-03 00:58:45 +0900

    HashRouter にしてみた

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { CssBaseline, Container } from "@material-ui/core";
 import { routes } from "../routes";
 import AppHeader from "./AppHeader";
