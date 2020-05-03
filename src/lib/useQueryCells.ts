6f890f40224a9f346406a5ef78047f0e3bee92d7
koyo-miyamura <nexus9071@gmail.com>
2020-05-03 22:53:39 +0900

    validate をちょっと関数に分けた

--- src/lib/useQueryCells.ts
+++ src/lib/useQueryCells.ts
@@ -3,30 +3,34 @@ import { defaultCells } from "./cells";
 import { cellsFromStr } from "./cellsConverter";
 import Game from "./lifegame";
 
+const validateCols = (cols: number): boolean => {
+  // NaN か 0 ならデフォルト値にする
+  if (!cols) {
+    return false;
+  }
+
+  return cols <= 100;
+};
+
+const validateCells = (cells: boolean[][]): boolean => {
+  return Game.rowLength(cells) <= 100 && Game.colLength(cells) <= 100;
+};
+
 export const useQueryCells = (): boolean[][] => {
   const query = new URLSearchParams(useLocation().search);
-  const cellsStr32 = query.get("cs");
-  const colsStr = query.get("c");
 
   // どちらかがクエリパラメータから取得できなければデフォルト値を返す
+  const cellsStr32 = query.get("cs");
+  const colsStr = query.get("c");
   if (!(cellsStr32 && colsStr)) {
     return defaultCells;
   }
 
-  // パースして NaN か 0 ならデフォルト値にする
   const cols = parseInt(colsStr, 10);
-  if (!cols) {
-    return defaultCells;
-  }
-  if (cols > 100) {
-    return defaultCells;
-  }
+  if (!validateCols(cols)) return defaultCells;
 
   const cells = cellsFromStr(cellsStr32, cols);
-
-  if (Game.rowLength(cells) > 100) {
-    return defaultCells;
-  }
+  if (!validateCells(cells)) return defaultCells;
 
   return cells;
 };
